52c4e6eb34465ea03c8db26005f691dcbac9b266
Zhanqian Wu <398290464@qq.com>
2024-02-07 15:02:40 -0500

    -

---
+++ HW4/water_tank.py
@@ -0,0 +1,18 @@
+# TODO: Students, fill out statement of work header
+# Student Name in Canvas:
+# Penn ID:
+# Did you do this homework on your own (yes / no):
+# Resources used outside course materials:
+
+# import statements
+from random import shuffle
+
+# TODO: Write the functions as described in the instructions
+
+def main():
+    # TODO: Write your code as described in the instructions
+	pass
+
+
+if __name__ == '__main__':
+    main()
